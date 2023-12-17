0f8fa3557a92bf9fd5a6fa2e66e9ca6ca61f4298
Origamologo <mituconservata@gmail.com>
2023-12-18 00:58:22 +0100

    funciones de visualización comentadas

--- empleados_package/visualizacion.py
+++ empleados_package/visualizacion.py
@@ -11,7 +11,23 @@ os.makedirs(IMAGES_PATH, exist_ok=True)
 
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=100):
 
+    """
+    Guarda una imagen en un archivo con opciones de formato y resolución especificadas.
 
+    Parámetros:
+    - fig_id (str): Identificador único para la imagen.
+    - tight_layout (bool): Controla si se aplica un diseño ajustado a la imagen.
+    - fig_extension (str): Extensión del archivo de la imagen (por ejemplo, "png", "jpg").
+    - resolution (int): Resolución de la imagen en puntos por pulgada (dpi).
+
+    Retorna:
+    - None: Guarda la imagen en un archivo con el nombre y formato especificados.
+
+    Comentarios:
+    - La función utiliza Matplotlib para guardar la imagen en un archivo en el directorio 'images'.
+    - 'fig_id' se utiliza para generar el nombre del archivo.
+    - 'tight_layout' controla si se aplica un diseño ajustado antes de guardar la imagen.
+    """
 
     path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
     print("Saving figure", fig_id)
@@ -21,6 +37,23 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=100):
 
 # Visualizacion de los datos numéricos en un heatmap
 def mapa_calor(dataframe, size=(30,20), nombre_fichero="heatmap"):
+
+    """
+    Crea un mapa de calor para visualizar las correlaciones entre variables numéricas en un DataFrame.
+
+    Parámetros:
+    - dataframe (pd.DataFrame): DataFrame que contiene los datos.
+    - size (tuple): Tamaño de la imagen (ancho, alto).
+    - nombre_fichero (str): Nombre del archivo para guardar la imagen.
+
+    Retorna:
+    - None: Muestra y guarda la grafica.
+
+    Comentarios:
+    - La función utiliza Seaborn y Matplotlib para crear un mapa de calor de las correlaciones entre columnas numéricas.
+    - El mapa de calor utiliza colores para representar las fuerzas y direcciones de las correlaciones.
+    """
+
     mask = np.triu(np.ones_like(dataframe.select_dtypes(include='number').corr(), dtype = bool))
     plt.figure(figsize=size)
     sns.heatmap(dataframe.select_dtypes(include='number').corr(), 
@@ -32,12 +65,47 @@ def mapa_calor(dataframe, size=(30,20), nombre_fichero="heatmap"):
 
 # Visualizacion de los datos numéricos en un histograma
 def histograma(dataframe, size=(20,15), nombre_fichero="histogram"):
+
+    """
+    Crea un histograma para visualizar la distribución de variables numéricas en un DataFrame.
+
+    Parámetros:
+    - dataframe (pd.DataFrame): DataFrame que contiene los datos.
+    - size (tuple): Tamaño de la imagen (ancho, alto).
+    - nombre_fichero (str): Nombre del archivo para guardar la imagen.
+
+    Retorna:
+    - None: Muestra y guarda la grafica.
+
+    Comentarios:
+    - La función utiliza la función 'hist' de Pandas para generar histogramas de las columnas numéricas.
+    """
+
     dataframe.hist(bins = 50, figsize = size)
     save_fig(nombre_fichero)
     plt.show()
 
 # Visualizacion de los datos categóricos en subplots de histogramas
 def categoricos(y, x, dataframe, size=(25,35), nombre_fichero="subplots"):
+
+    """
+    Crea subgráficos de barras horizontales para visualizar la distribución de variables categóricas en un DataFrame.
+
+    Parámetros:
+    - y (int): Número de filas de subgráficos.
+    - x (int): Número de columnas de subgráficos.
+    - dataframe (pd.DataFrame): DataFrame que contiene los datos.
+    - size (tuple): Tamaño de la imagen (ancho, alto).
+    - nombre_fichero (str): Nombre del archivo para guardar la imagen.
+
+    Retorna:
+    - None: Muestra y guarda la grafica.
+
+    Comentarios:
+    - Cada subgráfico representa la distribución de una columna categórica en el DataFrame.
+    - La función utiliza gráficos de barras horizontales para visualizar las frecuencias de categorías.
+    """
+
     fig, axes = plt.subplots(y, x, figsize = size)
 
     axes = axes.flat
@@ -55,6 +123,24 @@ def categoricos(y, x, dataframe, size=(25,35), nombre_fichero="subplots"):
 
 # Boxplot
 def boxplot(y, dataframe, size=(25,35), nombre_fichero="boxplot"):
+
+    """
+    Crea subgráficos de tipo boxplot para visualizar la distribución de variables numéricas en un DataFrame.
+
+    Parámetros:
+    - y (int): Número de subgráficos en la columna.
+    - dataframe (pd.DataFrame): DataFrame que contiene los datos.
+    - size (tuple): Tamaño de la imagen (ancho, alto).
+    - nombre_fichero (str): Nombre del archivo para guardar la imagen.
+
+    Retorna:
+    - None: Muestra y guarda la grafica.
+
+    Comentarios:
+    - La función utiliza Seaborn para crear boxplots.
+    - Cada subgráfico representa la distribución de una columna numérica en el DataFrame.
+    """
+
     fig, ax = plt.subplots(y, 1, figsize=size)
     columnas = dataframe.select_dtypes(include = "number").columns
     for i in range(len(columnas)):
